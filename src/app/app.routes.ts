a47b48870a9822ca76e2139571a6383b28c3496c
M4T3O0 <mateoortega431@gmail.com>
2025-10-06 16:53:28 -0300

    Modificar el icono del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { About } from './page/about/about';
+import { Contact } from './page/contact/contact';
+import { Product } from './page/product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: Home
+    },
+    {
+        path: "home", component: Home
+    },
+    {
+        path: "about", component: About
+    },
+    {
+        path: "contact", component: Contact
+    },
+    {
+        path: "product", component: Product
+    }
+
+];

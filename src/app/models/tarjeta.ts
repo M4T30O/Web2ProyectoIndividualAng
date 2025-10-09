9cdb0210d047ea0a1aad9b79513aacb4eda23552
mateoortega431@gmail.com <mateoortega431@gmail.com>
2025-10-09 16:03:02 -0300

    Creqacion de primera interfaz y uso directiva ng for

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,9 @@
+export interface Tarjeta {
+    id: string;
+    img: string;
+    alt: string;
+    nombreC: string;
+    titulo: string;
+    descripcion: string;
+    anualPublicacion: number;
+}
--- src/app/page/product/product.ts
+++ src/app/page/product/product.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Tarjeta } from '../../models/tarjeta';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css'
 })
 export class Product {
 
+  public infoTarjeta: Tarjeta[]
+
+
+  constructor() {
+    this.infoTarjeta = [
+      {
+        id: "12",
+        img: "../../../assets/caballo.jpg",
+        alt: "Foto de un caballo",
+        nombreC: "",
+        titulo: "Caballo",
+        descripcion: "Animal domesticado hace mas de 500 años, el cual formo parte de la vida humana por años",
+        anualPublicacion: 2025
+
+      },
+      {
+        id: "15",
+        img: "../../../assets/zorro.jpg",
+        alt: "Foto de un zorro",
+        nombreC: "Vulper Vulpes",
+        titulo: "Zorro",
+        descripcion: "Animal recientemente domesticado, se le relaciona en diferentes culturas y religiones como una señal de buen augurio",
+        anualPublicacion: 2025 
+      }
+    ]
+  }
 }
+
